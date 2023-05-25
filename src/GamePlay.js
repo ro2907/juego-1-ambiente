63d3a5813887a03885cbb69be11c914785f1a8ea
ro2907 <powerjujuy1@gmail.com>
2023-05-25 10:07:05 -0300

    inicio

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -16,7 +16,7 @@ GamePlayManager = {
     },
     preload: function() {
         game.load.image('background', 'assets/images/background.png');
-        game.load.spritesheet('horse', 'assets/images/horse.png', 84, 168, 2);
+        game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2);
         game.load.spritesheet('diamonds', 'assets/images/diamonds.png', 81, 84, 4);
         
         game.load.image('explosion', 'assets/images/explosion.png');
